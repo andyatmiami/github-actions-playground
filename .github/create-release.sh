23665c56d2911fdb7600153f70a5a62c46fc7f37
Andy Stoneberg <astonebe@redhat.com>
2024-12-02 14:24:02 -0500

    playing around with inspecting branches in the workflow

--- .github/create-release.sh
+++ .github/create-release.sh
@@ -1,3 +1,21 @@
 #! /usr/bin/env bash
 
-env
+# _get_target_branch()
+# {
+#     local tag="${1:-}"
+
+#     local tag_parts=($(printf "%s" "${tag}" | tr "." " "))
+#     local target_branch="${tag_parts[0]}.${tag_parts[1]}-branch"
+
+#     printf "%s" "${target_branch}"
+# }
+
+# target_branch=$(_get_target_branch "$GITHUB_REF_NAME")
+
+# gh release create "${GITHUB_REF_NAME}" \
+#     --repo="$GITHUB_REPOSITORY" \
+#     --title="${GITHUB_REF_NAME}" \
+#     --target "${target_branch}" \
+#     --generate-notes
+
+git branch -r --sort=-committerdate --list "*/v*"
